e268cc7747eed152973e93e1796403a82c184a23
murali <murali@gmail.com>
2019-02-07 22:36:08 +0530

    SpringBoot-CalculatorService committed

---
+++ SpringBoot-CalculatorService/src/main/java/com/mmr/spring/SpringBootCalculatorServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mmr.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCalculatorServiceApplication.class, args);
+	}
+
+}
+
